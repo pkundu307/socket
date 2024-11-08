bc6232448a8571a903df1d69cd5eb458ac2247c4
pkundu307 <pkundu307@gmail.com>
2024-11-08 12:20:39 +0530

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    var options = {
+        root: path.join(__dirname),
+        dotfiles: 'deny',
+        index: ['index.html']
+    }
+    res.sendFile('index.html', options);
+});
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+    
+    setTimeout(function() {
+        socket.emit('myCustomEvent',{description:'Hello world! Send me a message'});
+    }, 3);
+
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+    });
+});
+
+http.listen(3000, function() {
+    console.log('====================================');
+    console.log('Server listening on port 3000');
+    console.log('====================================');
+});
